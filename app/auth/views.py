9e3cfec0a38f287f5221f41949e3561a3ae39515
benjamin muchemi <benjamin.muchemi@student.moringaschool.com>
2021-08-15 20:41:21 +0300

    Create new auth template folder and create register.html

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,23 @@
-from flask import render_template
+from flask import render_template,redirect,url_for
+from .forms import RegistrationForm
 from . import auth
+from .. import db
+from ..models import User
+
 
 @auth.route('/login')
 def login():
     return render_template('auth/login.html')
+
+
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        mail_message('Welcome to pitch', 'email/welcome_user', user.email, user=user)
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)
