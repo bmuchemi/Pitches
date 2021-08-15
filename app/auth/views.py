9ea0fb2f05a16dfeaf85608a28148799b8207dbf
benjamin muchemi <benjamin.muchemi@student.moringaschool.com>
2021-08-15 22:27:24 +0300

    Craete two email.txt files and added message user would recieve when he or she registers

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,6 +4,7 @@ from flask_login import login_user,logout_user,login_required
 from . import auth
 from .. import db,photos
 from ..models import User
+from ..email import mail_message
 
 
 @auth.route('/login')
@@ -21,6 +22,8 @@ def login():
     return render_template('auth/login.html',login_form = login_form,title=title)
 
 
+
+
 @auth.route('/register',methods = ["GET","POST"])
 def register():
     form = RegistrationForm()
@@ -28,7 +31,7 @@ def register():
         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
-        mail_message('Welcome to pitch', 'email/welcome_user', user.email, user=user)
+        mail_message('Welcome to pitches', 'email/welcome_user', user.email, user=user)
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)
---
+++ start.sh
@@ -0,0 +1,3 @@
+export MAIL_USERNAME=bmuchemi55@gmail.com
+export MAIL_PASSWORD=BENJA9262
+python3.8 manage.py server
