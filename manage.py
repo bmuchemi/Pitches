065948990fcc31e28c6591dc80e928cc4527f793
benjamin muchemi <benjamin.muchemi@student.moringaschool.com>
2021-08-15 20:05:06 +0300

    Create a shell inside the app

--- manage.py
+++ manage.py
@@ -1,6 +1,24 @@
 from app import create_app,db
-from app.models import User
+from app.models import Comments, User,Pitches
 from flask_script import Manager, Server
+from  flask_migrate import Migrate, MigrateCommand
 
-app = create_app('prod')
+app = create_app('development')
 
+manager = Manager(app)
+manager.add_command('server', Server)
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
+@manager.command
+def test():
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User, Comments = Comments, Pitches = Pitches )
+
+if __name__ == '__main__':
+    manager.run()
